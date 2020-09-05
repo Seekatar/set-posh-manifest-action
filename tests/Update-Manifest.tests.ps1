114ba12cd042cce478aef02911fa0dd641b92da2
Jim Wallace <seekatar@gmail.com>
2020-09-05 09:19:08 -0400

    update pester

--- tests/Update-Manifest.tests.ps1
+++ tests/Update-Manifest.tests.ps1
@@ -10,6 +10,7 @@ Describe "Tests Manifest Version" {
         ..\Update-ManifestVersion.ps1 -ManifestPath $temp -Major 9 -Minor 8 -Build 7 -Revision 6
 
         $manifest = Test-ModuleManifest -Path $temp -Verbose:$false
+        Write-Warning "$($manifest.Version) $([Version]"9.8.7.6")"
         $manifest.Version | Should -be ([Version]"9.8.7.6")
     }
 
@@ -21,6 +22,7 @@ Describe "Tests Manifest Version" {
         ..\Update-ManifestVersion.ps1 -ManifestPath $temp -Major 9 -Minor 8 -Build 7
 
         $manifest = Test-ModuleManifest -Path $temp -Verbose:$false
+        Write-Warning "$($manifest.Version) $([Version]"9.8.7")"
         $manifest.Version | Should -be ([Version]"9.8.7")
     }
 
@@ -32,6 +34,7 @@ Describe "Tests Manifest Version" {
         ..\Update-ManifestVersion.ps1 -ManifestPath $temp -Major 9 -Minor 8
 
         $manifest = Test-ModuleManifest -Path $temp -Verbose:$false
+        Write-Warning "$($manifest.Version) $([Version]"9.8.0")"
         $manifest.Version | Should -be ([Version]"9.8.0")
     }
 
@@ -43,6 +46,7 @@ Describe "Tests Manifest Version" {
         ..\Update-ManifestVersion.ps1 -ManifestPath $temp -Revision 99
 
         $manifest = Test-ModuleManifest -Path $temp -Verbose:$false
+        Write-Warning "$($manifest.Version) $([Version]"0.1.0.99")"
         $manifest.Version | Should -be ([Version]"0.1.0.99")
     }
 
@@ -54,6 +58,7 @@ Describe "Tests Manifest Version" {
 
         ..\Update-ManifestVersion.ps1 -ManifestPath $temp -Build 99
 
+        Write-Warning "$($manifest.Version) $([Version]"0.1.99")"
         $manifest = Test-ModuleManifest -Path $temp -Verbose:$false
         $manifest.Version | Should -be ([Version]"0.1.99")
     }
