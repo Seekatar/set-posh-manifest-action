eb19bc482263e1d0c003a0849288cbddffd9e306
Jim Wallace <seekatar@gmail.com>
2020-09-04 20:38:58 -0400

    flesh out update

--- Update-ManifestVersion.ps1
+++ Update-ManifestVersion.ps1
@@ -26,13 +26,48 @@ param(
     # [ValidateScript( {Test-Path $_ -PathType Leaf})]
     [Parameter(Mandatory)]
     [string] $ManifestPath,
-    [Parameter(Mandatory)]
-    [int] $Revision,
-    [int] $Build = -1,
+    [int] $Major = -1,
     [int] $Minor = -1,
-    [int] $Major = -1
+    [int] $Build = -1,
+    [int] $Revision = -1
 )
 
 Set-StrictMode -Version Latest
+$manifest = Test-ModuleManifest -Path $ManifestPath -Verbose:$false
 
-Write-Verbose "$ManifestPath $Major.$Minor.$Build.$Revision"
+$verFormat = "{0}.{1}.{2}.{3}"
+if ( $Revision -eq -1 )
+{
+    $Revision = $manifest.Version.Revision
+}
+if ( $Revision -eq -1 )
+{
+    $verFormat = "{0}.{1}.{2}"
+}
+
+if ( $Build -eq -1 )
+{
+    $Build = $manifest.Version.Build
+}
+if ( $Build -eq -1 )
+{
+    $verFormat = "{0}.{1}"
+}
+if ( $Minor -eq -1 )
+{
+    $Minor = $manifest.Version.Minor
+}
+if ( $Major -eq -1 )
+{
+    $Major = $manifest.Version.Major
+}
+$newVersion = $verformat -f $Major, $Minor, $Build, $Revision
+
+if ( $PSCmdlet.ShouldProcess($ManifestPath, "Set module version to $newVersion"))
+{
+    Update-ModuleManifest -Path $ManifestPath -ModuleVersion $newVersion -Verbose:$false
+    # strip trailing whitespace the above command adds
+    $temp = New-TemporaryFile
+    Get-Content $ManifestPath | ForEach-Object { $_.TrimEnd(); } | Out-File -Append -FilePath $temp -Encoding utf8
+    Copy-Item $temp $ManifestPath
+}
